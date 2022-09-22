29036f73018afbe87085036d2d9f883ddf6d3b06
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-09-22 08:30:51 +0200

    Task 2

---
+++ Task 2/Task2.java
@@ -0,0 +1,15 @@
+class Task2 {
+
+	public static void main(String[] args) {
+
+		// 2.a Lav en metode, printPartOfWord(), med tre parametre: 1. parameter er ordet, 2. parameter er index for det bogstav delmængden starter med,
+		// og 3. parameter er længden på delmængden Ex: argumenterne "København", 1 og 4 skal give outputtet "øben".
+
+		printPartOfWord("Kobenhavn", 1, 4);
+
+	}
+
+	public static void printPartOfWord(String word, int index, int length) {
+		System.out.println(word.substring(index, length + 1));
+	}
+}
